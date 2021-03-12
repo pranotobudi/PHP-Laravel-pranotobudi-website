8e425aa0f32d2fede52750e22343fc583e0b79f5
Pranoto Budi <pranotobudi.s@gmail.com>
2021-03-13 06:06:42 +0700

    build profile website

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', 'PagesController@home');
+// Route::get('/', 'PagesController@home');
+// or
+// Route::get('/', 'App\Http\Controllers\PagesController@home');
+Route::get('/', [PagesController::class, 'home']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/contact', [PagesController::class, 'contact']);
